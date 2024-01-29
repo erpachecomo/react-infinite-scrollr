6c9921366a2a7383bb93ce6916ee0d0b1272eb28
erpachecomo <epachecomorelos@gmail.com>
2024-01-28 20:46:00 -0800

    remove dist

--- dist/esm/types/components/InfiniteScroller/InfiniteScroller.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { InfiniteScrollrProps } from "./InfiniteScrollr.types";
-declare const InfiniteScrollr: React.FC<InfiniteScrollrProps>;
-export default InfiniteScrollr;
